d09917cd7dd7679dd76936539a1a91fcf80e70fc
Grant Rutherford <contact.grantr@gmail.com>
2019-05-25 20:03:50 +0100

    sections and divs added to video page

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -3,10 +3,18 @@ import React from "react";
 export default function Video({ title, subtitle, dark, id }) {
   return (
     <div className={"section" + (dark ? " section-dark" : "")}>
-      <div className="section-content" id={id}>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-      </div>
+      <section className="section-content-video" id={id}>
+        <div className="div-content-video-grid-main">
+            <div className="div=content-video-grid">
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+            </div>
+            <div className="div=content-video-grid">
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
