0c53d970fcace4b56127ef1f135e48b0c21e8847
Grant Rutherford <contact.grantr@gmail.com>
2019-05-25 20:20:23 +0100

    flex and grid arranged for video section

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -3,18 +3,28 @@ import React from "react";
 export default function Video({ title, subtitle, dark, id }) {
   return (
     <div className={"section" + (dark ? " section-dark" : "")}>
-      <section className="section-content-video" id={id}>
-        <div className="div-content-video-grid-main">
-            <div className="div=content-video-grid">
-                <h1>{title}</h1>
-                <p>{subtitle}</p>
+        <section className="section-content-video" id={id}>
+            <div className="div-content-video-grid-main">
+                <div className="div-content-video-grid-duel">
+                    <div className="div-content-video-grid-single">
+                        <h1>{title}</h1>
+                        <p>{subtitle}</p>
+                    </div>
+                    <div className="div-content-video-grid-single">
+                        <p>{subtitle}</p>
+                    </div>
+                </div>
+                <div className="div-content-video-grid-duel">
+                    <div className="div-content-video-grid-single">
+                        <h1>{title}</h1>
+                        <p>{subtitle}</p>
+                    </div>
+                    <div className="div-content-video-grid-single">
+                        <p>{subtitle}</p>
+                    </div>
+                </div>
             </div>
-            <div className="div=content-video-grid">
-                <h1>{title}</h1>
-                <p>{subtitle}</p>
-            </div>
-        </div>
-      </section>
+        </section>
     </div>
   );
 }
